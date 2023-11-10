e031ea3020591bc98069dd8d006ed0eeb425875c
Emma Rothwell <emma.rothwell20@imperial.ac.uk>
2023-11-10 14:24:14 +0000

    Tensor-ified output.

--- 1D_equilibria_finder.py
+++ 1D_equilibria_finder.py
@@ -6,9 +6,12 @@ import itertools
 def payoff_finder_one_dim(num_positions, num_players):
     G = nx.grid_graph([num_positions])
     distance_dict = dict(nx.all_pairs_shortest_path_length(G))
-    outcome_list = []
     position_list = [i for i in range(num_positions)]
     position_permutations = np.array([list(product) for product in itertools.product(position_list, repeat=num_players)])
+    # going to have to create individual payoff tensors for my own sanity here
+    player_tensor = np.zeros((num_positions,) * num_players)
+    outcome_list = [player_tensor.copy() for i in range(num_players)]
+
     for player_positions in position_permutations:
         outcome = np.array([0 for i in range(num_players)], dtype="float")
         for spot in position_list:
@@ -21,7 +24,6 @@ def payoff_finder_one_dim(num_positions, num_players):
                 elif distance_dict[spot][player_positions[i]] == closest_distance:
                     closest_players.append(i)
             outcome[closest_players] += 1/(len(closest_players) * num_players)
-        outcome_list.append((player_positions, outcome))
-    print(outcome_list)
-        
+        for j in range(num_players):
+            outcome_list[j][tuple(player_positions)] += outcome[j]
 payoff_finder_one_dim(3, 3)
