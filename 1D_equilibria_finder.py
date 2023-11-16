3f410198355656f9cb09ac40944aab2d5b7d82d1
Emma Rothwell <emma.rothwell20@imperial.ac.uk>
2023-11-16 21:51:01 +0000

    Neatening up and commenting.

--- 1D_equilibria_finder.py
+++ 1D_equilibria_finder.py
@@ -1,15 +1,21 @@
-import networkx as nx 
-import numpy as np 
-import matplotlib.pyplot as plt
+import networkx as nx
+import numpy as np
 import itertools
 
 
 def payoff_finder_one_dim(num_positions, num_players):
+    """
+    Find an individual payoff tensor for each player on the num_positions
+    election grid.
+    """
+    # creating a graph that represents the positions and distance between them
     G = nx.grid_graph([num_positions])
     distance_dict = dict(nx.all_pairs_shortest_path_length(G))
     position_list = [i for i in range(num_positions)]
+    # obtaining all permutations for the player positions
     position_permutations = np.array([list(product) for product in itertools.product(position_list, repeat=num_players)])
-    # going to have to create individual payoff tensors for my own sanity here
+
+    # creating an individual tensor per player
     player_tensor = np.zeros((num_positions,) * num_players)
     outcome_list = [player_tensor.copy() for i in range(num_players)]
 
@@ -29,35 +35,42 @@ def payoff_finder_one_dim(num_positions, num_players):
                     else:
                         closest_players[player_positions[i]] = [i]
             for key in closest_players.keys():
+                # spltting the outcome so that if we have 2 left and 1 right, the left gets 2.5% each and the right 5%
                 outcome[closest_players[key]] += 1/(len(closest_players.keys()) * len(closest_players[key]) * num_positions)
         for j in range(num_players):
             outcome_list[j][tuple(player_positions)] += outcome[j]
     return outcome_list
 
-print(payoff_finder_one_dim(10, 3)[0])
+
 def get_best_response_for_players(num_positions, num_players):
+    """
+    Find the best response for a player, for each permutation of all other
+    players' positions
+    """
     outcome_list = payoff_finder_one_dim(num_positions, num_players)
     response_list = [[] for i in range(num_players)]
     position_list = [i for i in range(num_positions)]
     # get all the permutations of other players
     position_permutations = np.array([list(product) for product in itertools.product(position_list, repeat=num_players-1)])
     for player in range(num_players):
-    # this is ok for player a but for later players this is confusing -> put slice(None) where they have to be
         for position in position_permutations:
+            # slice(None) takes the entire column
             new_list = list(position)[0:player] + [slice(None)] + list(position)[player:]
             possible_outcomes = outcome_list[player][tuple(new_list)]
-            # print(possible_outcomes)
-            # print(np.argwhere(possible_outcomes == np.max(possible_outcomes)))
-            best_response = np.argwhere(possible_outcomes == np.max(possible_outcomes)) # TODO: this only gets one best response, of course there can be multiple
+            # gets all best responses possible, had to use isclose due to floating point errors
+            best_response = np.argwhere(np.isclose(possible_outcomes, np.max(possible_outcomes), 1e-6))
             for val in best_response:
                 new_list = list(position)[0:player] + [val[0]] + list(position)[player:]
                 response_list[player].append(new_list)
     return response_list
 
- # print(get_best_response_for_players(10, 2))
 
 def get_equilibria_for_players(num_positions, num_players):
-    # basically checks if the best responses intersect (if we have say [0, 1, 2] in all 3 players' best responses lists)
+    """
+    Obtain the equilibria for the game, given the number of players and
+    positions available.
+    """
+    # this checks if the best responses intersect
     response_list = get_best_response_for_players(num_positions, num_players)
     equilibrium_list = []
     for response in response_list[0]:
@@ -69,10 +82,11 @@ def get_equilibria_for_players(num_positions, num_players):
         if add_to_list:
             equilibrium_list.append(response)
     equilibrium_count = len(equilibrium_list)
-    return ([equilibrium for equilibrium in equilibrium_list]), equilibrium_count
+    return [equilibrium for equilibrium in equilibrium_list], equilibrium_count
 
-print(get_equilibria_for_players(10, 4))
-player_list = [2, 3, 4, 5, 6, 7]
+
+# obtaining values for the table seen in the report
+player_list = [2, 3, 4, 5]
 num_spaces = [10, 11]
 for player in player_list:
     for spaces in num_spaces:
@@ -80,4 +94,13 @@ for player in player_list:
         eq, eq_count = get_equilibria_for_players(spaces, player)
         print(eq)
         print(eq_count)
+        eq_set = []
+        # obtains the unique equilibria (permutations discarded)
+        for equilibrium in eq:
+            x = np.sort(equilibrium)
+            if list(x) in eq_set:
+                pass
+            else:
+                eq_set.append(list(x))
+        print(eq_set)
         np.save(f"{player}-{spaces}", eq)
