ff8abcb7a93aad8d6d24bfabb92249c8f1a469d3
sebastianodutola <sebastianodutola@me.com>
2023-11-17 11:57:44 +0000

    reduced support enumeration game size so that completes in a reasonable time: we actually find 12 mixed equilibria

--- support_enumeration.py
+++ support_enumeration.py
@@ -1,12 +1,11 @@
 import nashpy as nash
 import nD_equilibria_finder as equib
 #Attempt to find mixed equilibria of a 2D small political game
-payoff = equib.payoff_finder(4,2,2)
+payoff = equib.payoff_finder(3,2,2)
 deg_game = nash.Game(payoff)
 eq = deg_game.support_enumeration()
 for eq in eq:
     print(eq)
-
 
 
 
