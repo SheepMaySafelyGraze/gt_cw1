711944d13c3d9e1dfdec4428f46a14576f128489
sebastianodutola <sebastianodutola@me.com>
2023-11-17 12:49:52 +0000

    finally fixed support_enumeration, needed to transpose matrices

--- support_enumeration.py
+++ support_enumeration.py
@@ -1,11 +1,15 @@
 import nashpy as nash
 import nD_equilibria_finder as equib
+import numpy as np
 #Attempt to find mixed equilibria of a 2D small political game
-payoff = equib.payoff_finder(3,2,2)
-deg_game = nash.Game(payoff)
+num_positions = 3
+payoff = equib.payoff_finder(num_positions,2,2)
+deg_game = nash.Game(payoff.transpose(), (np.ones([num_positions**2,num_positions**2])-payoff).transpose())
 eq = deg_game.support_enumeration()
 for eq in eq:
     print(eq)
+print(equib.find_equilibria(3,2,2))
+
 
 
 
